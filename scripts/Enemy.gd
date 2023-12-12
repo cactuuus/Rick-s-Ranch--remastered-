236910d1eb9e69b8278e366764da82323246a9c6
cactus <jacopo.bob.calvi@gmail.com>
2023-12-12 13:11:08 +0000

    Enemy update
    - added Vulture and Bison enemies
    - sprite now flips horizontally to face the player

--- scripts/Enemy.gd
+++ scripts/Enemy.gd
@@ -1,22 +1,29 @@
 extends CharacterBody2D
 
 @onready var health : Node2D = $HealthModule
-@onready var animation_player : AnimationPlayer = $AnimationPlayer
+@onready var animation_player : AnimationPlayer = $EnemyAnimationPlayer
+@onready var sprite : AnimatedSprite2D = $EnemySprite
 var player : Node2D
 @export var max_health : int
 @export var attack : int
 @export var speed : float
 
+func _ready():
+	health.setup(max_health)
+	add_to_group("enemies")
+
 func _physics_process(delta):
 	var target = player.global_position
-	velocity = global_position.direction_to(target).normalized() * speed * delta
+	var direction = global_position.direction_to(target).normalized()
+	velocity = direction * speed * delta
+	update_sprite_direction(direction)
 	var collision = move_and_collide(velocity)
 	if collision:
 		handle_collision(collision)
 
-func _ready():
-	health.setup(max_health)
-	add_to_group("enemies")
+func update_sprite_direction(direction):
+	sprite.flip_h = ceil(direction.x)
+	
 
 func hit(damage_taken: int):
 	health.take_damage(damage_taken)
